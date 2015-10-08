7de07de5beb80f824ba8915cb664ff1cae04fbf6
joonaoko <joona.koskivaara@gmail.com>
2015-10-08 14:55:27 +0300

    Uutis url korjaus

--- view/vuutinen.php
+++ view/vuutinen.php
@@ -1,4 +1,4 @@
-<div class="uutinen">
+<div>
    <?php
    include("model/mnewsArticle.php");
    include("model/mnewsArticleComments.php");
@@ -17,18 +17,20 @@
    <?php
    if(/*$_SESSION['taso']>2*/ 1>0) { // *** PLACEHOLDER
    ?>
-   <h3 class="sininen">Muokkaa artikkelia</h3>
-   <form action="model/mnewsModifyArticle.php?uutisid=<?php echo($uutinen->uutisID);?>" class="uutisen-muokkaus" method="post" id="modify-article-form-<?php echo($uutinen->uutisID);?>">
-      <input type="text" name="otsikko" placeholder="Otsikko" value="<?php echo($uutinen->otsikko);?>">
-      <br><br>
-      <textarea rows="16" cols="100" name="teksti" placeholder="Kirjoita tähän" form="modify-article-form-<?php echo($uutinen->uutisID);?>"><?php echo($uutinen->teksti);?></textarea>
-      <br>
-      <input type="submit" value="Tallenna muutokset" class="button">
-   </form>
-   <hr>
-   <?php
-   }
-   ?>
+   <div class="artikkeli-kirjoitus">
+      <h3 class="sininen">Muokkaa artikkelia</h3>
+      <form action="model/mnewsModifyArticle.php?uutisid=<?php echo($uutinen->uutisID);?>" class="uutisen-muokkaus" method="post" id="modify-article-form-<?php echo($uutinen->uutisID);?>">
+         <input type="text" name="otsikko" placeholder="Otsikko" value="<?php echo($uutinen->otsikko);?>">
+         <br><br>
+         <textarea rows="16" cols="100" name="teksti" placeholder="Kirjoita tähän" form="modify-article-form-<?php echo($uutinen->uutisID);?>"><?php echo($uutinen->teksti);?></textarea>
+         <br>
+         <input type="submit" value="Tallenna muutokset" class="orange button">
+      </form>
+      <hr>
+      <?php
+      }
+      ?>
+   </div>
 
    <section class="kommentit">
       <h3 class="sininen">Kommentit</h3>
@@ -50,7 +52,7 @@
                   if(/*$_SESSION['taso']>2*/ 1>0) { // *** PLACEHOLDER
                   ?>
                      <form action="model/mnewsArticleDeleteComment.php?kommenttiid=<?php echo($uutiskommentti->kommenttiid);?>" method="post">
-                        <input type="submit" value="Poista kommentti" class="button" data-uutisid="<?php echo($uutiskommentti->k_uutisID);?>">
+                        <input type="submit" value="Poista kommentti" class="orange button" data-uutisid="<?php echo($uutiskommentti->k_uutisID);?>">
                      </form>
                   <?php
                   }
@@ -73,7 +75,7 @@
          <br><br>
          <textarea rows="8" cols="100" name="teksti" placeholder="Kirjoita tähän" form="commentform-<?php echo($uutinen->uutisID);?>"></textarea>
          <br>
-         <input type="submit" value="Kommentoi" class="button" data-uutisid="<?php echo($uutinen->uutisID);?>">
+         <input type="submit" value="Kommentoi" class="orange button" data-uutisid="<?php echo($uutinen->uutisID);?>">
       </form>
       <?php
       } else {
--- view/vuutiset.php
+++ view/vuutiset.php
@@ -8,24 +8,26 @@
    <?php
    if(/*$_SESSION['taso']>2*/ 1>0) { // *** PLACEHOLDER
    ?>
-   <h3 class="sininen">Luo uusi artikkeli</h3>
-   <form action="model/mnewsAddArticle.php" class="uutisen-luonti" method="post" id="create-article-form-<?php echo($uutinen->uutisID);?>">
-      <input type="text" name="otsikko" placeholder="Otsikko">
-      <br><br>
-      <textarea rows="16" cols="100" name="teksti" placeholder="Kirjoita tähän" form="create-article-form-<?php echo($uutinen->uutisID);?>"></textarea>
-      <br>
-      <input type="submit" value="Luo artikkeli" class="button">
-   </form>
-   <?php
-   }
-   ?>
+   <div class="artikkeli-kirjoitus">
+      <h3 class="sininen">Luo uusi artikkeli</h3>
+      <form action="model/mnewsAddArticle.php" class="uutisen-luonti" method="post" id="create-article-form-<?php echo($uutinen->uutisID);?>">
+         <input type="text" name="otsikko" placeholder="Otsikko">
+         <br><br>
+         <textarea rows="16" cols="100" name="teksti" placeholder="Kirjoita tähän" form="create-article-form-<?php echo($uutinen->uutisID);?>"></textarea>
+         <br>
+         <input type="submit" value="Luo artikkeli" class="orange button">
+      </form>
+      <?php
+      }
+      ?>
+   </div>
 
    <?php
    foreach($uutiset as $uutinen){
    ?>
       <section class="uutinen">
-         <article>
-            <a href="<?php echo(SITE_ROOT)?>news.php?uutisid=<?php echo($uutinen->uutisID);?>"><h3><?php echo($uutinen->otsikko);?></h3></a>
+         <article class="main-content">
+            <a href="<?php echo(SITE_ROOT)?>uutiset/<?php echo($uutinen->uutisID);?>"><h3><?php echo($uutinen->otsikko);?></h3></a>
             <p><?php echo($uutinen->teksti);?></p>
             <p class="pvm"><?php echo($uutinen->pvm);?>
             <a href="#">Kommentoi</a>
@@ -34,7 +36,7 @@
             if(/*$_SESSION['taso']>2*/ 1>0) { // *** PLACEHOLDER
             ?>
                <form action="<?php echo(SITE_ROOT)?>model/mnewsDeleteArticle.php?uutisid=<?php echo($uutinen->uutisID);?>" method="post">
-                  <input type="submit" value="Poista artikkeli" class="button" data-uutisid="<?php echo($uutinen->uutisID);?>">
+                  <input type="submit" value="Poista artikkeli" class="orange button" data-uutisid="<?php echo($uutinen->uutisID);?>">
                </form>
             <?php
             }
