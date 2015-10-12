dbcea1dff0255ca9cb43c31c22bb80be0f50eae9
joonaoko <joona.koskivaara@gmail.com>
2015-10-12 14:30:15 +0300

    Uutiset w/ alustava CSS ~valmis

--- view/vuutinen.php
+++ view/vuutinen.php
@@ -1,104 +1,112 @@
-<div>
-   <?php
-   include("model/mnewsArticle.php");
-   include("model/mnewsArticleComments.php");
-   // print_r($uutinen);
-   ?>
-   <section class="uutinen">
-      <p><a href="<?php echo(SITE_ROOT)?>uutiset"><-- Back to news</a></p>
-      <article>
-         <h3><?php echo($uutinen->otsikko);?></h3>
-         <p><?php echo($uutinen->teksti);?></p>
-         <p class="pvm"><?php echo($uutinen->pvm);?>
-      </article>
-   </section>
-
-   <?php
-   if($_SESSION['taso']>2) {
-   ?>
-      <button type="button" name="button" class="button toggle-hidden orange" data-target="artikkeli-muokkaus">Muokkaa</button>
-      <hr>
-      <div id="artikkeli-muokkaus" style="display: none;">
-         <h3 class="sininen">Muokkaa artikkelia</h3>
-         <form action="<?php echo(SITE_ROOT)?>model/mnewsModifyArticle.php?uutisid=<?php echo($uutinen->uutisID);?>" class="uutisen-muokkaus" method="post" id="modify-article-form-<?php echo($uutinen->uutisID);?>">
-            <textarea rows="2" cols="100" name="otsikko" placeholder="Otsikko" form="modify-article-form-<?php echo($uutinen->uutisID);?>" required><?php echo($uutinen->otsikko);?></textarea>
-            <br>
-            <textarea rows="16" cols="100" name="teksti" placeholder="Kirjoita tähän" form="modify-article-form-<?php echo($uutinen->uutisID);?>" required><?php echo($uutinen->teksti);?></textarea>
-            <br>
-            <input type="submit" value="Tallenna muutokset" class="orange button">
-         </form>
-         <hr>
-      </div>
-   <?php
-   }
-   ?>
-
-   <section class="kommentit">
-      <h3 class="sininen">Comments</h3>
+<div class="uutiset">
+   <section class="main-content">
       <?php
-      if(empty($uutiskommentit)) {
+      include("model/mnewsArticle.php");
+      include("model/mnewsArticleComments.php");
+      // print_r($uutinen);
       ?>
-      <p>No comments yet.</p>
-      <?php
-      } else {
-         if(isset($_GET['show'])) {
-            $limit=$_GET['show'];
-         } else {
-            $limit=20;
-         }
-         foreach(array_reverse($uutiskommentit) as $uutiskommentti) {
-            $i++;
-            if ($i>$limit) {
-               break;
-            } else {
-      ?>
-            <section class="uutinen">
-               <article class="kommentti">
-                  <a href="#"><?php echo($uutiskommentti->username);?></a>
-                  <h4><?php echo($uutiskommentti->otsikko);?></h4>
-                  <p><?php echo($uutiskommentti->teksti);?></p>
-                  <p class="pvm"><?php echo($uutiskommentti->pvm);?></p>
-                  <?php
-                  if($_SESSION['taso']>2) {
-                  ?>
-                     <form action="<?php echo(SITE_ROOT)?>model/mnewsArticleDeleteComment.php?kommenttiid=<?php echo($uutiskommentti->kommenttiid);?>&uutisid=<?php echo($uutinen->uutisID);?>" method="post" class="delete-form">
-                        <input type="submit" value="Poista kommentti" class="red button" data-uutisid="<?php echo($uutiskommentti->k_uutisID);?>">
-                     </form>
-                  <?php
-                  }
-                  ?>
-               </article>
-            </section>
+      <section class="uutinen">
+         <p><a href="<?php echo(SITE_ROOT)?>uutiset"><-- Back to news</a></p>
+         <article>
+            <h3><?php echo($uutinen->otsikko);?></h3>
+            <br>
+            <p><?php echo($uutinen->teksti);?></p>
+            <br>
+            <p class="pvm"><?php echo($uutinen->pvm);?>
             <hr>
+         </article>
+      </section>
+
       <?php
+      if($_SESSION['taso']>2) {
+      ?>
+         <button type="button" name="button" class="button toggle-hidden orange" data-target="artikkeli-muokkaus">Muokkaa</button>
+         <hr>
+         <div id="artikkeli-muokkaus" style="display: none;">
+            <h3 class="sininen">Muokkaa artikkelia</h3>
+            <form action="<?php echo(SITE_ROOT)?>model/mnewsModifyArticle.php?uutisid=<?php echo($uutinen->uutisID);?>" class="uutisen-muokkaus" method="post" id="modify-article-form-<?php echo($uutinen->uutisID);?>">
+               <textarea rows="2" cols="100" name="otsikko" placeholder="Otsikko" form="modify-article-form-<?php echo($uutinen->uutisID);?>" required><?php echo($uutinen->otsikko);?></textarea>
+               <br>
+               <textarea rows="16" cols="100" name="teksti" placeholder="Kirjoita tähän" form="modify-article-form-<?php echo($uutinen->uutisID);?>" required><?php echo($uutinen->teksti);?></textarea>
+               <br>
+               <input type="submit" value="Tallenna muutokset" class="orange button">
+            </form>
+            <hr>
+         </div>
+      <?php
+      }
+      ?>
+
+      <section class="kommentit">
+         <h3 class="sininen">Comments</h3>
+         <?php
+         if(empty($uutiskommentit)) {
+         ?>
+         <p>No comments yet.</p>
+         <?php
+         } else {
+            if(isset($_GET['show'])) {
+               $limit=$_GET['show'];
+            } else {
+               $limit=20;
+            }
+            foreach(array_reverse($uutiskommentit) as $uutiskommentti) {
+               $i++;
+               if ($i>$limit) {
+                  break;
+               } else {
+         ?>
+               <section class="uutinen">
+                  <article class="kommentti">
+                     <div class="posted-by">Posted by: <a href="#"><?php echo($uutiskommentti->username);?></a></div>
+                     <br>
+                     <h4><?php echo($uutiskommentti->otsikko);?></h4>
+                     <p><?php echo($uutiskommentti->teksti);?></p>
+                     <br>
+                     <p class="pvm"><?php echo($uutiskommentti->pvm);?></p>
+                     <?php
+                     if($_SESSION['taso']>2) {
+                     ?>
+                        <form action="<?php echo(SITE_ROOT)?>model/mnewsArticleDeleteComment.php?kommenttiid=<?php echo($uutiskommentti->kommenttiid);?>&uutisid=<?php echo($uutinen->uutisID);?>" method="post" class="delete-form">
+                           <input type="submit" value="Poista kommentti" class="red button" data-uutisid="<?php echo($uutiskommentti->k_uutisID);?>">
+                        </form>
+                     <?php
+                     }
+                     ?>
+                  </article>
+               </section>
+               <hr>
+               <br>
+         <?php
+               }
+            }
+            if(sizeof($uutiskommentit)>$limit) {
+         ?>
+         <a name="showmore" href="<?php echo(SITE_ROOT)?>index.php?osio=uutiset&uutisid=<?php echo($uutinen->uutisID);?>&show=<?php echo($limit+20);?>#showmore">Displaying up to <?php echo($limit);?> comments. Show more:</a>
+         <?php
             }
          }
-         if(sizeof($uutiskommentit)>$limit) {
-      ?>
-      <a name="showmore" href="<?php echo(SITE_ROOT)?>index.php?osio=uutiset&uutisid=<?php echo($uutinen->uutisID);?>&show=<?php echo($limit+20);?>#showmore">Displaying up to <?php echo($limit);?> comments. Show more:</a>
-      <?php
+         ?>
+
+         <a name=comment><h3 class="sininen">Comment</h3></a>
+
+         <?php
+         if($_SESSION['taso']>1) {
+         ?>
+         <form action="<?php echo(SITE_ROOT)?>model/mnewsArticleAddComment.php?uutisid=<?php echo($uutinen->uutisID);?>" class="kommentti" method="post" id="commentform-<?php echo($uutinen->uutisID);?>">
+            <textarea rows="1" cols="80" name="otsikko" placeholder="Title" form="commentform-<?php echo($uutinen->uutisID);?>"></textarea>
+            <br>
+            <textarea rows="8" cols="80" name="teksti" placeholder="Type here" form="commentform-<?php echo($uutinen->uutisID);?>" required></textarea>
+            <br>
+            <input type="submit" value="Comment" class="orange button" data-uutisid="<?php echo($uutinen->uutisID);?>">
+         </form>
+         <?php
+         } else {
+         ?>
+            <p><a href="#" class="open-modal" data-target="log-modal">Log in to comment.</a></p>
+         <?php
          }
-      }
-      ?>
-
-      <a name=comment><h3 class="sininen">Comment</h3></a>
-
-      <?php
-      if($_SESSION['taso']>1) {
-      ?>
-      <form action="<?php echo(SITE_ROOT)?>model/mnewsArticleAddComment.php?uutisid=<?php echo($uutinen->uutisID);?>" class="kommentti" method="post" id="commentform-<?php echo($uutinen->uutisID);?>">
-         <textarea rows="1" cols="80" name="otsikko" placeholder="Title" form="commentform-<?php echo($uutinen->uutisID);?>"></textarea>
-         <br>
-         <textarea rows="8" cols="80" name="teksti" placeholder="Type here" form="commentform-<?php echo($uutinen->uutisID);?>" required></textarea>
-         <br>
-         <input type="submit" value="Comment" class="orange button" data-uutisid="<?php echo($uutinen->uutisID);?>">
-      </form>
-      <?php
-      } else {
-      ?>
-         <p><a href="#" class="open-modal" data-target="log-modal">Log in to comment.</a></p>
-      <?php
-      }
-      ?>
+         ?>
+      </section>
    </section>
 </div>
--- view/vuutiset.php
+++ view/vuutiset.php
@@ -1,55 +1,60 @@
 <div class="uutiset">
-   <h2>News</h2>
-   <?php
-      include("model/mnews.php");
-      // print_r($uutiset);
-   ?>
+   <section class="main-content">
+      <div class="news-section-title"><h2>News</h2></div>
+      <?php
+         include("model/mnews.php");
+         // print_r($uutiset);
+      ?>
 
-   <?php
-   if($_SESSION['taso']>2) {
-   ?>
-      <a href="<?php echo(SITE_ROOT)?>uutiset/uusi_artikkeli/luo"><button type="button" name="button" class="button orange">Luo uusi artikkeli</button></a>
-   <?php
-   }
-   ?>
-
-   <?php
-   if(isset($_GET['show'])) {
-      $limit=$_GET['show'];
-   } else {
-      $limit=10;
-   }
-   foreach(array_reverse($uutiset) as $uutinen){
-      $i++;
-      if ($i>$limit) {
-         break;
-      } else {
-   ?>
-         <section class="uutinen">
-            <article class="main-content">
-               <h3><a href="<?php echo(SITE_ROOT)?>uutiset/<?php echo($uutinen->uutisID);?>"><?php echo($uutinen->otsikko);?></a></h3>
-               <p><?php echo($uutinen->teksti);?></p>
-               <p class="pvm"><?php echo($uutinen->pvm);?>
-               <p><a href="<?php echo(SITE_ROOT)?>uutiset/<?php echo($uutinen->uutisID);?>#comment">Comment</a></p>
-               <?php
-               if($_SESSION['taso']>2) {
-               ?>
-                  <form action="<?php echo(SITE_ROOT)?>model/mnewsDeleteArticle.php?uutisid=<?php echo($uutinen->uutisID);?>" method="post" class="delete-form">
-                     <input type="submit" value="Poista artikkeli" class="red button" data-uutisid="<?php echo($uutinen->uutisID);?>">
-                  </form>
-               <?php
-               }
-               ?>
-            </article>
-         </section>
-         <hr>
+      <?php
+      if($_SESSION['taso']>2) {
+      ?>
+         <a href="<?php echo(SITE_ROOT)?>uutiset/uusi_artikkeli/luo"><button type="button" name="button" class="button orange">Luo uusi artikkeli</button></a>
       <?php
       }
-   }
-   if(sizeof($uutiset)>$limit) {
-   ?>
-   <a name="showmore" href="<?php echo(SITE_ROOT)?>index.php?osio=uutiset&show=<?php echo($limit+10);?>#showmore">Displaying up to <?php echo($limit);?> articles. Show more:</a>
-   <?php
-   }
-   ?>
+      ?>
+
+      <?php
+      if(isset($_GET['show'])) {
+         $limit=$_GET['show'];
+      } else {
+         $limit=10;
+      }
+      foreach(array_reverse($uutiset) as $uutinen){
+         $i++;
+         if ($i>$limit) {
+            break;
+         } else {
+      ?>
+            <section class="uutinen">
+               <article>
+                  <h3 class="article-title"><a href="<?php echo(SITE_ROOT)?>uutiset/<?php echo($uutinen->uutisID);?>"><?php echo($uutinen->otsikko);?></a></h3>
+                  <br>
+                  <p><?php echo($uutinen->teksti);?></p>
+                  <br>
+                  <div class="pvm"><?php echo($uutinen->pvm);?></div>
+                  <a class="comment-link" href="<?php echo(SITE_ROOT)?>uutiset/<?php echo($uutinen->uutisID);?>#comment">Comment</a>
+                  <br>
+                  <?php
+                  if($_SESSION['taso']>2) {
+                  ?>
+                     <form action="<?php echo(SITE_ROOT)?>model/mnewsDeleteArticle.php?uutisid=<?php echo($uutinen->uutisID);?>" method="post" class="delete-form">
+                        <input type="submit" value="Poista artikkeli" class="red button" data-uutisid="<?php echo($uutinen->uutisID);?>">
+                     </form>
+                  <?php
+                  }
+                  ?>
+               </article>
+            </section>
+            <hr><br>
+         <?php
+         }
+      }
+      if(sizeof($uutiset)>$limit) {
+      ?>
+         <a name="showmore" href="<?php echo(SITE_ROOT)?>index.php?osio=uutiset&show=<?php echo($limit+10);?>#showmore">Displaying up to <?php echo($limit);?> articles. Show more:</a>
+      <?php
+      }
+      ?>
+   </section>
 </div>
