3248e424426204649d7a1eca138ce2cf3e35dd4a
rikutimonen <Riku.Timonen@metropolia.fi>
2015-10-13 15:26:39 +0300

    footer muutettu hienoks

--- view/footer.php
+++ view/footer.php
@@ -1,8 +1,19 @@
 <footer class = "dark-gray">
-	<p>footer</p>
+	<p>
+	contact: mrbadger@badger.com
 	<?php 
-	print_r($_SESSION['username']);
+	if ($_SESSION['kirjautunut']){
+		echo('<br> Logged in as ' . $_SESSION['username']);
+	}
+	if ($_SESSION['taso'] > 2) {
+		echo('<br>Admin level: ');
+		print_r($_SESSION['taso']);
+		echo('<br>');
+		print_r($_SESSION['email']);
+	}
+	/*print_r($_SESSION['username']);
 	print_r($_SESSION['taso']);
-	print_r($_SESSION['kirjautunut']);
+	print_r($_SESSION['kirjautunut']);*/
 	?>
+	</p>
 </footer>
