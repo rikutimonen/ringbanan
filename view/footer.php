71e4a30ddb99b95083c34bb59264b8494bea4cbd
joonaoko <joona.koskivaara@gmail.com>
2015-10-13 15:35:05 +0300

    ei-sininen, footer

--- view/footer.php
+++ view/footer.php
@@ -1,15 +1,15 @@
 <footer class = "dark-gray">
 	<p>
 	contact: mrbadger@badger.com
-	<?php 
+	<?php
 	if ($_SESSION['kirjautunut']){
 		echo('<br> Logged in as ' . $_SESSION['username']);
 	}
 	if ($_SESSION['taso'] > 2) {
-		echo('<br>Admin level: ');
-		print_r($_SESSION['taso']);
+		echo('<br>User level: ');
+		echo($_SESSION['taso'] . ' (Admin)');
 		echo('<br>');
-		print_r($_SESSION['email']);
+		echo('<p>' . $_SESSION['email'] . '</p>');
 	}
 	/*print_r($_SESSION['username']);
 	print_r($_SESSION['taso']);
